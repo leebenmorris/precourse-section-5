5f41576b5672c5f4f5f3c689988b6d1d5d044747
Lee Morris <leebenmorris@gmail.com>
2017-01-23 01:33:02 +0000

    Bugs squashed, code refactored and commented

--- main.js
+++ main.js
@@ -2,134 +2,178 @@
 $(document).ready(function(){
 
   let guessChar = ''
-  let p1Word = ''
-  let p1WordLength = 0
+  let phrase = ''
   let errorCount = 0
   let success = 0
   let successCount = 0
-  let displayBlank = ''
   let spaceCount = 0
+  let hanged = false
   let canvas = document.getElementById("myCanvas").getContext("2d")
 
-  $('#p1Word').focus()
+  $('#phrase').focus()  //focus on input box when page first loads
+  prepGuessArea(' ')    //put a space in the guessArea to prevent page layout altering later
 
-  $('#p1Btn').click(function(){
-    canvas.clearRect(0, 0, 200, 200)
-    canvas.beginPath()
+  //submit button click capture
+  $('#submitBtn').click(function(){
+
+    //reset everything on the page when the submit button is clicked
+    $('#phrase').focus()              //refocus on input box
+    canvas.clearRect(0, 0, 200, 200)  //clear canvas area
+    canvas.beginPath()                //clear path cache
     errorCount = 0
     successCount = 0
-    p1Word = $('#p1Word').val().toUpperCase()
-    p1WordLength = p1Word.length
-    $('#p1Word').val('')
-    $('#guessArea').empty()
-    $('#keys button').removeClass('disabled').on('click')
+    $('#phrase').val('')              //blank the input box so player 2 cannot see entered phrase
+    $('#guessArea').empty()           //clear all previous elements from guessArea
+    $('#keys button').off('click')    //remove click handlers from on screen keyboard
+    $('#keys button').removeClass('disabled').on('click')  //reset keyboard and enable click handlers
 
+    //capture the input phrase from the input field
+    phrase = $('#phrase').val().toUpperCase()
 
-    spaceCount = 0
-    for(let i = 0; i < p1WordLength; i++) {
-      if(p1Word[i] === ' '){
-        displayBlank = ' '
-        spaceCount++
-      } else {
-        displayBlank = '_'
-      }
-      $('#guessArea').append('<span id="char' + i + '">' + displayBlank + '</span>')
-    }
+    //put blanks in guessArea and record the number of spaces in the phrase
+    spaceCount = prepGuessArea(phrase)
 
+    //onscreen keyboard click capture
     $('#keys button').click(function(){
+
+      //capture keypress and store
       guessChar = $(this).text()
 
+      //change colour of clicked key and disable further clicks
       $(this).addClass('disabled').off('click')
 
-      success = 0
-      for(let i = 0; i < p1WordLength; i++) {
-        if(p1Word[i] === guessChar) {
-          $('#char' + i).text(guessChar)
-          success++
-        }
-      }
+      //fill guessArea with 1 or more characters from phrase that match keypress
+      //and record how many characters where successfully guessed for a single
+      //keyboard press (eg success = 2 if phrase has two A's)
+      success = fillGuessArea(phrase,guessChar)
 
+      //update counters
       if(success === 0) errorCount++
       else successCount += success
 
-      if(successCount === p1WordLength - spaceCount) alert('Player 2 Wins\nPlease enter a new word to start again')
+      //has player 2 won?
+      //timeouts used to ensure correct operation of page
+      if(successCount === phrase.length - spaceCount) {
+        setTimeout(() => alert('GAME OVER!\nPlayer 2 wins!\nPlease enter a new word to start again'), 50)
+        setTimeout(() => $('#phrase').focus(), 1)
+      }
 
-      drawHangMan()
+      //draw hangman and record if complete
+      hanged = drawHangMan(canvas,errorCount)
 
-      console.log(guessChar)
-      console.log('success', success)
-      console.log('errorCount', errorCount)
-      console.log('successCount', successCount)
+      //has player 1 won?
+      //timeouts used to ensure correct operation of page
+      if(hanged === true) {
+        setTimeout(() => alert('GAME OVER!\nPlayer 1 wins!\nPlease enter a new word to start again'), 50)
+        setTimeout(() => $('#phrase').focus(), 1)
+      }
+
+      //console tests to help debug
+      //console.log(guessChar)
+      //console.log('success', success)
+      //console.log('errorCount', errorCount)
+      //console.log('successCount', successCount)
+
       return false
     })
 
-
-    console.log(p1WordLength)
-    console.log('success', success)
-    console.log('errorCount', errorCount)
-    console.log('successCount', successCount)
+    //console tests to help debug
+    //console.log(phrase, phrase.length)
+    //console.log('success', success)
+    //console.log('errorCount', errorCount)
+    //console.log('successCount', successCount)
 
     return false
   })
 
-
-// ******* FUNCTIONS ********* //
-  function drawHangMan() {
-    switch(errorCount) {
-      case 1:
-        //bottom beam
-        canvas.moveTo(10,190)
-        canvas.lineTo(150,190)
-        break
-      case 2:
-        //vertical beam
-        canvas.moveTo(20,195)
-        canvas.lineTo(20,5)
-        break
-      case 3:
-        //top beam
-        canvas.moveTo(10,10)
-        canvas.lineTo(100,10)
-        break
-      case 4:
-        //rope
-        canvas.moveTo(95,5)
-        canvas.lineTo(95,30)
-        break
-      case 5:
-        //head
-        canvas.moveTo(115,50)
-        canvas.arc(95,50,20,0,2*Math.PI)
-        break
-      case 6:
-        //body
-        canvas.moveTo(95,70)
-        canvas.lineTo(95,130)
-        break
-      case 7:
-        //right arm
-        canvas.moveTo(95,80)
-        canvas.lineTo(65,110)
-        break
-      case 8:
-        //left arm
-        canvas.moveTo(95,80)
-        canvas.lineTo(125,110)
-        break
-      case 9:
-        //right leg
-        canvas.moveTo(95,130)
-        canvas.lineTo(75,180)
-        break
-      case 10:
-        //left leg
-        canvas.moveTo(95,130)
-        canvas.lineTo(115,180)
-        alert('GAME OVER!\nPlayer 1 wins!\nPlease enter a new word to start again')
-        break
-      default:
-        break
-    }
-    canvas.stroke()
-  }
 })
+
+
+//********** FUNCTIONS **********//
+
+function prepGuessArea(string) {
+  let spaceCount = 0
+  let displayBlank = ''
+  for(let i = 0; i < string.length; i++) {
+    if(string[i] === ' '){
+      displayBlank = ' '
+      spaceCount++
+    } else {
+      displayBlank = '_'
+    }
+    $('#guessArea').append('<span id="char' + i + '">' + displayBlank + '</span>')
+  }
+  return spaceCount
+}
+
+function fillGuessArea(string,char) {
+  let success = 0
+  for(let i = 0; i < string.length; i++) {
+    if(string[i] === char) {
+      $('#char' + i).text(char)
+      success++
+    }
+  }
+  return success
+}
+
+function drawHangMan(c,e) {
+  let hanged = false
+  switch(e) {
+    case 1:
+      //bottom beam
+      c.moveTo(10,190)
+      c.lineTo(150,190)
+      break
+    case 2:
+      //vertical beam
+      c.moveTo(20,195)
+      c.lineTo(20,5)
+      break
+    case 3:
+      //top beam
+      c.moveTo(10,10)
+      c.lineTo(100,10)
+      break
+    case 4:
+      //rope
+      c.moveTo(95,5)
+      c.lineTo(95,30)
+      break
+    case 5:
+      //head
+      c.moveTo(115,50)
+      c.arc(95,50,20,0,2*Math.PI)
+      break
+    case 6:
+      //body
+      c.moveTo(95,70)
+      c.lineTo(95,130)
+      break
+    case 7:
+      //right arm
+      c.moveTo(95,80)
+      c.lineTo(65,110)
+      break
+    case 8:
+      //left arm
+      c.moveTo(95,80)
+      c.lineTo(125,110)
+      break
+    case 9:
+      //right leg
+      c.moveTo(95,130)
+      c.lineTo(75,180)
+      break
+    case 10:
+      //left leg
+      c.moveTo(95,130)
+      c.lineTo(115,180)
+      hanged = true
+      break
+    default:
+      break
+  }
+  c.stroke()
+  return hanged
+}
